12c4994a895e056bfdb137aae2ff8b91c81ec8d7
Jimmy Sandoval <jsandovaldev.pe@gmail.com>
2024-01-19 22:32:11 -0500

    feat: Update astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), icon()],
   image: {
-    service: passthroughImageService(),
-  },
+    service: passthroughImageService()
+  }
 });
